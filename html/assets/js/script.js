579931333e4cc322a643f9531b89c07e7c6d3275
Alejandro Ramos <99296122+imgiwu@users.noreply.github.com>
2022-03-18 14:01:15 -0600

    Add files via upload

---
+++ html/assets/js/script.js
@@ -0,0 +1,10 @@
+// line comment
+alert('line comment')
+
+/* block comment 
+more than one line
+- inline comment */
+
+console.log('Block comment')
+const fname = 'John Doe'
+const fname = 'Someone Else'
